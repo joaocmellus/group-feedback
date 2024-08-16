b8fb5444e78b41b55542dc27be9d3579a0decb46
Matheus Luna <srmatheus6162@gmail.com>
2024-08-16 16:22:13 -0300

    feat; xd

---
+++ js/data.js
@@ -0,0 +1,23 @@
+const funcionarios = [
+  {
+    userId: 1,
+    comentario: "Ótima dedicação ao trabalho, sempre disposto a ajudar.",
+    positivo: 1,
+  },
+  {
+    userId: 2,
+    comentario: "Precisa melhorar a pontualidade, atrasos frequentes.",
+    positivo: 0,
+  },
+  {
+    userId: 3,
+    comentario:
+      "Atende os clientes de forma educada, mas precisa melhorar a organização.",
+    positivo: 0,
+  },
+  {
+    userId: 4,
+    comentario: "Excelente trabalho em equipe, muito colaborativo.",
+    positivo: 1,
+  },
+];
