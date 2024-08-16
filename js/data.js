b04b1e89ff7b0c7c0571c1a34877d3b148417a3e
Matheus Luna <srmatheus6162@gmail.com>
2024-08-16 16:30:10 -0300

    Feat: tudo

--- js/data.js
+++ js/data.js
@@ -5,9 +5,19 @@ const funcionarios = [
     positivo: 1,
   },
   {
+    userId: 1,
+    comentario: "Precisa melhorar a comunicação com os colegas.",
+    positivo: 0,
+  },
+  {
     userId: 2,
     comentario: "Precisa melhorar a pontualidade, atrasos frequentes.",
     positivo: 0,
+  },
+  {
+    userId: 2,
+    comentario: "Desenvolveu um projeto inovador que melhorou a produtividade.",
+    positivo: 1,
   },
   {
     userId: 3,
@@ -16,8 +26,18 @@ const funcionarios = [
     positivo: 0,
   },
   {
+    userId: 3,
+    comentario: "Bom domínio das tarefas técnicas, sempre entrega no prazo.",
+    positivo: 1,
+  },
+  {
     userId: 4,
     comentario: "Excelente trabalho em equipe, muito colaborativo.",
     positivo: 1,
+  },
+  {
+    userId: 4,
+    comentario: "Precisa melhorar a gestão de tempo, prioriza mal as tarefas.",
+    positivo: 0,
   },
 ];
