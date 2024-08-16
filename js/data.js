f509230b616cdb1e68c72ca98e153c5f19b9510e
Matheus Luna <srmatheus6162@gmail.com>
2024-08-16 16:33:16 -0300

    feat: data foi

--- js/data.js
+++ js/data.js
@@ -1,4 +1,5 @@
 const funcionarios = [
+  // Comentários para userId: 1
   {
     userId: 1,
     comentario: "Ótima dedicação ao trabalho, sempre disposto a ajudar.",
@@ -10,6 +11,28 @@ const funcionarios = [
     positivo: 0,
   },
   {
+    userId: 1,
+    comentario: "Contribui de forma significativa para o ambiente de trabalho.",
+    positivo: 1,
+  },
+  {
+    userId: 1,
+    comentario: "Falta proatividade em algumas situações.",
+    positivo: 0,
+  },
+  {
+    userId: 1,
+    comentario: "Atendimento ao cliente impecável, muito atencioso.",
+    positivo: 1,
+  },
+  {
+    userId: 1,
+    comentario: "Demora para responder e-mails importantes.",
+    positivo: 0,
+  },
+
+  // Comentários para userId: 2
+  {
     userId: 2,
     comentario: "Precisa melhorar a pontualidade, atrasos frequentes.",
     positivo: 0,
@@ -19,6 +42,28 @@ const funcionarios = [
     comentario: "Desenvolveu um projeto inovador que melhorou a produtividade.",
     positivo: 1,
   },
+  {
+    userId: 2,
+    comentario: "Bom em resolver problemas técnicos complexos.",
+    positivo: 1,
+  },
+  {
+    userId: 2,
+    comentario: "Dificuldade em trabalhar sob pressão.",
+    positivo: 0,
+  },
+  {
+    userId: 2,
+    comentario: "Muito criativo na busca de soluções.",
+    positivo: 1,
+  },
+  {
+    userId: 2,
+    comentario: "Às vezes, tem dificuldade em seguir prazos.",
+    positivo: 0,
+  },
+
+  // Comentários para userId: 3
   {
     userId: 3,
     comentario:
@@ -31,6 +76,28 @@ const funcionarios = [
     positivo: 1,
   },
   {
+    userId: 3,
+    comentario: "Muito prestativo e educado no atendimento ao cliente.",
+    positivo: 1,
+  },
+  {
+    userId: 3,
+    comentario: "Precisa melhorar na organização dos documentos.",
+    positivo: 0,
+  },
+  {
+    userId: 3,
+    comentario: "É capaz de resolver problemas de forma eficiente.",
+    positivo: 1,
+  },
+  {
+    userId: 3,
+    comentario: "Tem dificuldade em trabalhar em equipe.",
+    positivo: 0,
+  },
+
+  // Comentários para userId: 4
+  {
     userId: 4,
     comentario: "Excelente trabalho em equipe, muito colaborativo.",
     positivo: 1,
@@ -38,6 +105,26 @@ const funcionarios = [
   {
     userId: 4,
     comentario: "Precisa melhorar a gestão de tempo, prioriza mal as tarefas.",
+    positivo: 0,
+  },
+  {
+    userId: 4,
+    comentario: "Sempre disposto a ajudar os colegas, muito atencioso.",
+    positivo: 1,
+  },
+  {
+    userId: 4,
+    comentario: "Às vezes, falta atenção aos detalhes.",
+    positivo: 0,
+  },
+  {
+    userId: 4,
+    comentario: "Grande capacidade de liderança e motivação.",
+    positivo: 1,
+  },
+  {
+    userId: 4,
+    comentario: "Pode ser um pouco resistente a mudanças.",
     positivo: 0,
   },
 ];
